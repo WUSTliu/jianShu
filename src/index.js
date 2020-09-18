0f0bf80ac1d97185d2fc2e1b15bbe72fd7101e7f
‘‘liuwh5_asiainfo’ <lwh992017@gmail.com>
2020-09-18 23:34:59 +0800

    提交简书小项目

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './style.js';
+import './statics/iconfont';
+
+ReactDOM.render(<App />, document.getElementById('root'));
